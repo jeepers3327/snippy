df0548db2d2dbd14e87625962b76d96b13294625
Jhefrey Sajot <heifurukawa1997@gmail.com>
2021-07-07 20:49:09 +0800

    configure app

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,40 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
+import * as passport from 'passport';
+import * as redis from 'redis';
+import * as connectRedis from 'connect-redis';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = process.env.APP_PORT || 3000;
+
+  const client = redis.createClient({ url: process.env.REDIS_URI });
+  const RedisStore = connectRedis(session);
+
+  app.use(helmet());
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.use(
+    session({
+      store: new RedisStore({ client }),
+      secret: process.env.APP_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        sameSite: 'lax',
+        maxAge: 3600000 * 24 * 14,
+        secure: process.env.NODE_ENV === 'production',
+      },
+    }),
+  );
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  await app.listen(PORT, '0.0.0.0');
 }
 bootstrap();
