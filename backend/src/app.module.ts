361408e18d26ed0230a0861bea8827146efa33ac
Jhefrey Sajot <heifurukawa1997@gmail.com>
2021-07-18 14:12:04 +0800

    chore: imports all feature modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,11 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { GistsModule } from './gists/gists.module';
+import { SearchModule } from './search/search.module';
+import { FilesModule } from './files/files.module';
+import { StargazersModule } from './stargazers/stargazers.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
   imports: [
@@ -10,6 +15,11 @@ import { UsersModule } from './users/users.module';
     PrismaModule,
     AuthModule,
     UsersModule,
+    GistsModule,
+    SearchModule,
+    FilesModule,
+    StargazersModule,
+    CommentsModule,
   ],
 })
 export class AppModule {}
